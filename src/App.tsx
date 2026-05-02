a7aa7a3835c2c4d08708b32caf267f5a8f969afb
Chieri Richling <chieri1427@gmail.com>
2026-05-02 15:19:05 -0600

    use shadcn/ui and lucide icons

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,33 @@
 import './App.css'
+import { useState } from 'react'
 import ProjectTile from './components/ProjectTile'
-import reactLogo from './assets/react.svg'
-import viteLogo from './assets/vite.svg'
+import reactLogo from '@/assets/react.svg'
+import viteLogo from '@/assets/vite.svg'
+import { Button } from '@/components/ui/button'
+import { Moon, Sun } from 'lucide-react'
 
 function App() {
 
+  // Load dark/light from saved local storage or get system preference
+  const [dark, setDark] = useState<boolean>(() => localStorage.getItem("theme") === "dark"
+  || (!localStorage.getItem("theme") && window.matchMedia("(prefers-color-scheme: dark)").matches));
+
+  const toggleDark = () => {
+    const next = !dark;
+    setDark(next);
+    document.documentElement.classList.toggle("dark", next);
+    localStorage.setItem("theme", next ? "dark" : "light");
+  }
+
   return (
     <>
+         <Button className="h-[30px] w-[30px] px-0 gap-0" onClick={toggleDark}>
+          {dark ? <Sun className="flex-1" size={30}/> : <Moon className="flex-1" size={30}/>}
+         </Button>
          <ProjectTile 
-          title="cool project" 
+          title="Cool project" 
           imageSrc={reactLogo} 
-          description="Look at this super cool project!"
+          description="Look at this super cool project! Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur."
           skills={["React", "TypeScript", "Vite"]}
           link="coolproject.com"
          />
@@ -18,7 +35,7 @@ function App() {
          <ProjectTile 
           title="Another cool project" 
           imageSrc={viteLogo} 
-          description="This one is also cool"
+          description="This one is also cool... Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur."
           skills={["Vite"]}
           link="coolproject2.com"
          />
--- src/components/ProjectTile.tsx
+++ src/components/ProjectTile.tsx
@@ -1,3 +1,12 @@
+import {
+    Card,
+    CardTitle,
+    CardContent,
+} from "@/components/ui/card"
+import {Button} from "@/components/ui/button"
+import {Badge} from "@/components/ui/badge"
+import {SquareArrowOutUpRight} from "lucide-react"
+
 interface ProjectTileProps {
     title: string; 
     imageSrc: any; 
@@ -9,17 +18,24 @@ interface ProjectTileProps {
 
 export default function ProjectTile({title, imageSrc, description, skills, link}: ProjectTileProps) {
     return (
-        <>
-            <h2>{title}</h2>
-            <img src={imageSrc} alt={title} style={{width: "100px"}}/>
-            <div className="description">
-                <p>{description}</p>
-            </div>
-            <div className="skills">
-                <p>Skills: {skills.join(" ")}</p>
-            </div>
-            
-            {link && <a className="link" href={link}>Link</a>}
-        </>
+        <Card className="w-4xl">
+            <CardTitle className="px-4 text-lg">{title}</CardTitle> 
+            <CardContent className="flex flex-row gap-6">
+                <img src={imageSrc} alt={title} className="flex-1"/>
+                <div className="flex-3 flex flex-col gap-4">
+                    <div className="description">
+                        <p>{description}</p>
+                    </div>
+                    <div className="skills">
+                        Skills {skills.map((skill: string) => <Badge variant="secondary" key={skill}>{skill}</Badge>)}
+                    </div>
+                    {link && <Button asChild className="w-[60px]">
+                        <a href={link}>
+                            Link <SquareArrowOutUpRight />
+                        </a>
+                    </Button>}
+                </div>
+            </CardContent>
+        </Card>
     )
 }
